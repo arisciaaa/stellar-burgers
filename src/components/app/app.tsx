a454946f03f2d7901f9046b193c690b102a9b716
Арина Скобелева <am.skobeleva@yandex.ru>
2026-08-26 11:04:00 +0300

    fixed critical error

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -129,6 +129,19 @@ const App = () => {
           }
         />
 
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+
+        <Route path='/feed/:number' element={<OrderInfo />} />
+
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <ProtectedRoute>
+              <OrderInfo />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path='*' element={<NotFound404 />} />
       </Routes>
 
